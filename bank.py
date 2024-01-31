dd79d7c013818380bfb3d1fa92ed32e35317abc6
Angela Davis <angela__davis@outlook.com>
2024-01-31 22:31:14 +0000

    First draft of bank.py

---
+++ bank.py
@@ -0,0 +1,12 @@
+#bank.py
+#This program asks a user to enter two amounts of money in cents, adds the amounths together and prints the answer in Euros
+#Author: Angela Davis
+
+amount1 = int(input("Enter amount1(in cents): "))
+amount2 = int(input("Enter amount2(in cents): "))
+
+totalCents = (amount1 + amount2)
+totalEuro = (totalCents / 100)
+
+print (f"The sum of these is €{totalEuro}")
+
