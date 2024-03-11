860e77ea8b3f8d998ac830018ad6d7444ed0c8c0
Angela Davis <angela__davis@outlook.com>
2024-03-11 17:24:11 +0000

    Improvements made for weekly task 7 e_counter.py to include reading filename from command line and converting text to lower case to avoid case causing errors

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 
 # Extra reading used for weekly task 5 weekday.py:
 # https://docs.python.org/3/library/datetime.html
+
+# Extra reading used for weekly task 7 e_counter.py:
+# https://docs.python.org/3/library/sys.html#sys.argv
+# https://docs.python.org/3/library/os.path.html#os.path.splitext
+# https://docs.python.org/3/tutorial/errors.html#handling-exceptions
+#
--- e_counter.py
+++ e_counter.py
@@ -1,42 +1,48 @@
 # e_counter.py
-# Write a program that reads in a text file and outputs the number of e's it contains. Think about what is being asked here, document any assumptions you are making.
+# Write a program that reads in a text file and outputs the number of e's it contains.
 # The program should take the filename from an argument on the command line.
-# Marks will be given for dealing with errors eg no argument, filename that does not exist, or is not a text file.
 # Author: Angela Davis
+
 #------------------------------------------------------------------------------------------------------------------------#
-# Assumptions starting: 
+# Brainstorming: 
 # As starting point - Have multiple text files w/ various numbers of e characters already existing to test with > done
 # Create a function for counting the e's in the chosen file > done
 # Assuming the file will be in the current directory/folder, input is used to take in the filename from the user > done
 # create error catching for:
-        # 1. blank feedback from user > started
-        # 2. incorrect/non existing filename > started
-        # 3. asked for a file which is not a text file > to be started
-#------------------------------------------------------------------------------------------------------------------------#
-# Need to check: 
-        # need to check how to determine file type (or are we just expecting user to enter .txt at end) 
-        # can we read in a file from a user that is located in a different folder/directory
+        # 1. blank feedback from user > done
+        # 2. incorrect/non existing filename > done
+        # 3. asked for a file which is not a text file > done
 #------------------------------------------------------------------------------------------------------------------------#
 
-# first version with counting e's in a file already created w/ static file name not taken in from user
+# Assumptions: 
+# File the user will enter is using the relative path name if the same directory as the program or full path name if the file exists in another location, program should be able to handle both 
+# E should be counted whether it is uppercase or lowercase
+# Since the file is being entered in the command line when the user is triggering the program it is assumed that error handly should end the program without retry and print a human readable error for the user 
+
+#------------------------------------------------------------------------------------------------------------------------#
+
 
 import os.path
-FILENAME = input("Please enter the filename: ") # this sentences takes the filename in from the user and assigns it to FILENAME
+import sys 
+
+FILENAME = sys.argv[1]
+file_title, file_extension = os.path.splitext(FILENAME.lower()) # .lower() is used in case the user types the filetype in all caps to convert to lower case for the error handling below
 
 def count_E(): # this fuction takes in the file defined as the FILENAME above. Reads the content and counts the e's in the file.
     try: 
         with open(FILENAME) as f: # file is first attemped to be opened as "f" using the input from the user
             full_file = f.read()  # the content of the file is set as variable full_file
-            number_of_E = full_file.count("e") # the count method is used to scan the text and count each e
-            return number_of_E # the number of es are then returned from the function
+            number_of_E = full_file.lower().count("e") # lower() is used to convert the file to all lowercase and then count the e's to ensure no uppercase e's are left out
+            print (f"There were {number_of_E} e's") # The number of Es are printed for the user
         
-    except IOError: # if the file cannot be opened using the input from the user 
+    except: # if the file cannot be opened using the input from the user 
         if FILENAME == "": # first we check if the feedback from the user was blank
-            print("You have not entered any filename.")
+            print("No filename has been entered.\nProgram exited.")
 
+        elif file_extension != ".txt":
+            print(f"The filename you have entered indicates your file is a {file_extension} file.\n This program only accepts .txt files.\nProgram exited.")
+            
         else: 
-            print("Filename provided does not exist.") # else we assume the users input was not matching a filename that exists  
+            print("Filename provided does not exist.\nProgram exited.") # else we assume the users input was not matching a filename that exists  
 
-num_of_E = count_E() # function is called and output set to num_of_E
-
-print (f"There were {num_of_E} e's") # The number of Es are printed for the user
+count_E() # function is called
