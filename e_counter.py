cfe3fc41a6933025f6299baf475a2e2e67c9cd24
Angela Davis <angela__davis@outlook.com>
2024-03-11 11:59:01 +0000

    weekly task 7 file created & brainstorming for assumptions before starting

---
+++ e_counter.py
@@ -0,0 +1,15 @@
+# e_counter.py
+# Write a program that reads in a text file and outputs the number of e's it contains. Think about what is being asked here, document any assumptions you are making.
+# The program should take the filename from an argument on the command line.
+# Marks will be given for dealing with errors eg no argument, filename that does not exist, or is not a text file.
+# Author: Angela Davis
+
+# Assumptions starting: 
+# Pre requisite - Have multiple text files to test with w/ various numbers of e characters
+# Create a function for counting the e's in the chosen file
+# use input for to take in the file name from the user > might need to look into this further
+# create error catching for 
+# 1. blank feedback from user
+# 2. incorrect/non existing filename 
+# 3. asked for a file which is not a text file
+
---
+++ squareroot.py
@@ -0,0 +1,14 @@
+# squareroot.py
+# This takes a positive floating-point number as input and outputs an approximation of its square root.
+# create a function called <tt>sqrt</tt> that does this.
+# create your own sqrt function and not to use the built in functions x ** .5 or math.sqrt(x).
+# look at the newton method at estimating square roots. 
+
+
+
+'''
+output example:
+$ python squareroot.py
+Please enter a positive number: 14.5
+The square root of 14.5 is approx. 3.8.
+'''
